dfca0caf7d7e71b80d52b47afb4d33320bceb3fd
AbdallahTaha1 <engbody007@gmail.com>
2023-07-05 12:39:37 +0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,63 @@
+// ========================================================== //
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+// ========================================================== //
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+// ========================================================== //
+// Smooth Scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// ========================================================== //
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
---
+++ README.md
@@ -0,0 +1,17 @@
+# Build Responsive Real-World Websites with HTML and CSS
+
+- This is my project the I built with this course
+- It was great experience and interesting journy
+
+## Projects
+
+### Omnifood
+
+- [Project-code](https://github.com/AbdallahTaha1/Omnifood-project) <br>
+- [Live Demo](https://omnifood-taha.netlify.app/)
+
+---
+
+[Course-Link](https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3)<br>
+
+[Certificate](https://www.udemy.com/certificate/UC-50273288-7974-4598-bbde-bcde86c0b7f1/)
